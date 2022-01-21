1cfba12ba751f51be9879569402b9a50f48e7dc2
RVaman <94143580+RVaman@users.noreply.github.com>
2022-01-21 16:02:46 -0500

    Fixed Color Sensor gradle import error
    Removed color sensor lib from project

--- 2022-RedWatch-RobotCode/src/main/java/frc/robot/subsystems/ColorDetection.java
+++ 2022-RedWatch-RobotCode/src/main/java/frc/robot/subsystems/ColorDetection.java
@@ -6,8 +6,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
 import static frc.robot.Constants.ColorConstants.*;
