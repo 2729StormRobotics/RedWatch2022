0f4db6f1ce98e825185d30c115b33fb2f113d2f6
Josh <94143587+JoshuaO22@users.noreply.github.com>
2022-02-21 13:00:43 -0500

    Change shuffleboard tab name
    to avoid interference from other shuffleboard tabs

--- RedWatch2022/src/main/java/frc/robot/Constants.java
+++ RedWatch2022/src/main/java/frc/robot/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
     public static final int RIGHT_MOTOR_ID = 0;
     public static final boolean kLeftReversedDefault = true;
     public static final boolean kRightReversedDefault = !kLeftReversedDefault;
-    public static final String kShuffleboardTab = "Control Panel";
+    public static final String kShuffleboardTab = "Hanger Panel";
     public static final int LEFT_MOTOR2_ID = 14;
     public static final int RIGHT_MOTOR2_ID = 1;
     public static final int STALL_LIMIT = 45;
