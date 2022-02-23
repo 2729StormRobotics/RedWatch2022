d8b5671452f8cc0ed238638f873908523321f655
Josh <94143587+JoshuaO22@users.noreply.github.com>
2022-02-23 15:22:27 -0500

    remove unused imports

--- RedWatch2022/src/main/java/frc/robot/RobotContainer.java
+++ RedWatch2022/src/main/java/frc/robot/RobotContainer.java
@@ -12,9 +12,7 @@ import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commandgroups.Traverse;
-import frc.robot.commands.differentialDrive;
 import frc.robot.commands.hangerControl;
-import frc.robot.commands.hangerRunMotors;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
