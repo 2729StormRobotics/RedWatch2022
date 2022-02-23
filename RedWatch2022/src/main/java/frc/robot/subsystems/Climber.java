6c6fa7e7723b1cec1fb77417c76f4e0efa051430
Clam <57159181+133622@users.noreply.github.com>
2022-02-23 15:44:15 -0500

    Update Climber.java

--- RedWatch2022/src/main/java/frc/robot/subsystems/Climber.java
+++ RedWatch2022/src/main/java/frc/robot/subsystems/Climber.java
@@ -58,7 +58,7 @@ public class Climber extends SubsystemBase {
     setMotor(m_climbLeftPivot);
     setMotor(m_climbRightPivot);
 
-    m_climbLeftExtend.follow(m_climbRightExtend, false);
+    m_climbLeftExtend.follow(m_climbRightExtend, false); // false for opposite; true for same direction
     m_climbLeftPivot.follow(m_climbRightPivot, true);
 
     m_climbRightEncoder = m_climbRightExtend.getEncoder();
