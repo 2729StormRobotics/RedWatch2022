107e8e67a44684c8dde2030c5bead14add724305
JoshuaO22 <94143587+JoshuaO22@users.noreply.github.com>
2022-02-24 01:18:40 -0500

    Remove shuffleboard stuff from Light subsystem

--- RedWatch2022/src/main/java/frc/robot/subsystems/Lights.java
+++ RedWatch2022/src/main/java/frc/robot/subsystems/Lights.java
@@ -26,22 +26,22 @@ public class Lights extends SubsystemBase {
   private final Spark m_ledDriver;
   private final NetworkTable m_lightTable;
   private final Timer m_timeToSpeed = new Timer();
-  private final ShuffleboardTab m_ShuffleboardTab;
-  private final ShuffleboardLayout m_lightValues;
+  // private final ShuffleboardTab m_ShuffleboardTab;
+  // private final ShuffleboardLayout m_lightValues;
 
 
 
   /** Creates a new Lights. */
   public Lights() {
-    m_ShuffleboardTab = Shuffleboard.getTab(Constants.kShuffleboardTab);
-    m_lightValues = m_ShuffleboardTab.getLayout("Light Jawndess", BuiltInLayouts.kList);
+    // m_ShuffleboardTab = Shuffleboard.getTab(Constants.kShuffleboardTab);
+    // m_lightValues = m_ShuffleboardTab.getLayout("Light Jawndess", BuiltInLayouts.kList);
     m_lightTable = NetworkTableInstance.getDefault().getTable("Light Statuses");
 
 
     m_ledDriver = new Spark(Constants.Ports.kBlinkinDriverPort);
     resetLights();
 
-    m_lightValues.addNumber("Light Output", () -> getCurrentLights());
+    // m_lightValues.addNumber("Light Output", () -> getCurrentLights());
   }
 
   /* TODO: When robot takes in ball of each color
