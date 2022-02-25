62dfdffd14a3763fae34d2663bd0404dac63e5a9
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2022-02-25 15:09:24 -0500

    Merge branch 'Dev' into Shooter

--- RedWatch2022/src/main/java/frc/robot/commands/RevToSpeed.java
+++ RedWatch2022/src/main/java/frc/robot/commands/RevToSpeed.java
@@ -4,21 +4,22 @@
 
 package frc.robot.commands;
 
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.Shooter;
 
 public class RevToSpeed extends CommandBase {
-
-  Shooter m_shooter;
   Lights m_lights;
-  double m_motorPower;
-  double currentRPM;
+  Shooter m_shooter;
+  double m_motorPower = 0;
+  double currentRPM = 0;
   double m_TargetRPM;
-  double increment;
-  double error;
+  double increment = 0;
+  double error = 0;
+  boolean finished = false;
 
   /** Creates a new RevToSpeed. */
   public RevToSpeed(double rpm, Shooter shooter, Lights lights) {
@@ -26,6 +27,7 @@ public class RevToSpeed extends CommandBase {
     m_TargetRPM = rpm;
     m_shooter = shooter;
     m_lights = lights;
+
     double m_motorPower = 0;
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -35,7 +37,11 @@ public class RevToSpeed extends CommandBase {
   public void initialize() {
     m_shooter.topMotor.set(0);
     m_shooter.encoderReset(m_shooter.m_topEncoder);
-    m_lights.RevFlyWheelYellow();
+    m_motorPower = 0;
+    currentRPM = 0;
+    increment = 0;
+    error = 0;
+    finished = false;
     
     
   }
@@ -46,23 +52,29 @@ public class RevToSpeed extends CommandBase {
     currentRPM = m_shooter.getEncoderVelocity(m_shooter.m_topEncoder);
     error = m_TargetRPM - currentRPM;
 
-    increment = error/100000;
+    increment = error / 120000;
+    m_shooter.increment = increment;
     m_motorPower += increment;
+    m_shooter.motorPower = m_motorPower;
 
     m_shooter.topMotor.set(m_motorPower);
+
+    finished = Math.abs(error) <= 20;
+    
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_shooter.flyWheelSpeedAfterRev = m_motorPower;
-    m_lights.MaxSpeedFlyWheel();
+    m_shooter.topMotor.set(0);
+
 
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return Math.abs(currentRPM - m_TargetRPM) <= 0.005;
+    return finished;
   }
 }
