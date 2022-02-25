e785c858e664125941a8ef30d10b41a94e11e8c8
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-02-25 17:20:22 -0500

    Updated Constants.java

--- RedWatch2022/src/main/java/frc/robot/Constants.java
+++ RedWatch2022/src/main/java/frc/robot/Constants.java
@@ -133,11 +133,10 @@ public final class Constants {
     public static class LightConstants {
 		public static final double kDisabled = 0.0; //TODO: Find what color we want for this and its value
 		public static final double kLightsOff = 0.99;
-        public static final double kRedBall = 0.61;
+        public static final double kRed = 0.61; // Red color used for both red ball and a bad orientation
         public static final double kBlueBall = 0.87;
         public static final double kDefaultColor = 0.93; //TODO: Find what we want default to be (same as disabled?)
         public static final double kCorrect = 0.75; //Defines Dark Green
-        public static final double kBad = 0.61; //Defines Red for bad orrientation
     }
     
     public static final class ClimberConstants {
--- RedWatch2022/src/main/java/frc/robot/subsystems/Lights.java
+++ RedWatch2022/src/main/java/frc/robot/subsystems/Lights.java
@@ -50,7 +50,7 @@ public class Lights extends SubsystemBase {
   }
 
   public void intakeRed() {
-    m_ledDriver.set(kRedBall);
+    m_ledDriver.set(kRed);
   }
 
   public void intakeBlue() {
@@ -72,7 +72,7 @@ public class Lights extends SubsystemBase {
 
   // Show's red when the bot is not orientated correctly under the rung. 
   public void badOrientation() {
-    m_ledDriver.set(kBad);
+    m_ledDriver.set(kRed);
   }
 
   public void setGiven(double color) {
