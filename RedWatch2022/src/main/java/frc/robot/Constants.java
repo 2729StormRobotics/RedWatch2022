bb65ee4e70ed4e479538f967fafb86c5b46ee366
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-02-25 17:30:35 -0500

    Update Constants.java

--- RedWatch2022/src/main/java/frc/robot/Constants.java
+++ RedWatch2022/src/main/java/frc/robot/Constants.java
@@ -93,7 +93,7 @@ public final class Constants {
     public static class LightConstants {
 		public static final double kDisabled = 0.0; //TODO: Find what color we want for this and its value
 		public static final double kLightsOff = 0.99;
-        public static final double kRedBall = 0.61; // Intake red ball //TODO Do we want
+        public static final double kRedBall = 0.61; // Intake red ball //TODO Do we want flashing?
         public static final double kBad = 0.61; // Used for a bad orientation - color red
         public static final double kBlueBall = 0.87;
         public static final double kDefaultColor = 0.93; //TODO: Find what we want default to be (same as disabled?)
