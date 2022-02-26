579de129c688de0504611530a95f21c80dd7a458
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-02-25 23:27:06 -0500

    Update Shooter.java

--- RedWatch2022/src/main/java/frc/robot/subsystems/Shooter.java
+++ RedWatch2022/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,8 +4,8 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ShooterConstants.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
