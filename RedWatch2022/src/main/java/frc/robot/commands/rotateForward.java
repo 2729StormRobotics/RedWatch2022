19d4c62357c28d6b6d4d6968c98a537ecd3d367d
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-02-27 01:46:05 -0500

    Update rotateForward.java

--- RedWatch2022/src/main/java/frc/robot/commands/rotateForward.java
+++ RedWatch2022/src/main/java/frc/robot/commands/rotateForward.java
@@ -19,7 +19,6 @@ public class rotateForward extends CommandBase {
     // Use addRequirements() here to declare subsystem dependencies.
     m_climber = climber;
     addRequirements(m_climber);
-
   }
 
   // Called when the command is initially scheduled.
