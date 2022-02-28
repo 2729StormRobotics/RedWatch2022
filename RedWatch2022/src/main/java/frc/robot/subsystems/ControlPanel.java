68b60291bc08a69422ea066b36086d33f5af9b6f
AkshayM1 <94143669+AkshayM1@users.noreply.github.com>
2022-02-28 15:24:03 -0500

    fixed auto route 1

--- RedWatch2022/src/main/java/frc/robot/subsystems/ControlPanel.java
+++ RedWatch2022/src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -176,7 +176,10 @@ public class ControlPanel extends SubsystemBase {
     Shuffleboard.selectTab(Constants.kShuffleboardTab);
   }
 
-  @Override
+  public ControlPanel(Shooter m_shooter, Indexer m_indexer, Lights m_lights) {
+}
+
+@Override
   public void periodic() {
     // This method will be called once per scheduler run
     if (isBallRed.getAsBoolean()) {
