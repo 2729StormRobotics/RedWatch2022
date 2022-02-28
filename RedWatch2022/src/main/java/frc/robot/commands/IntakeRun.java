fb29d880fd2ed6e52d94374b4c99a3a51a288867
Clam <57159181+133622@users.noreply.github.com>
2022-02-28 14:42:25 -0500

    Update IntakeRun.java

--- RedWatch2022/src/main/java/frc/robot/commands/IntakeRun.java
+++ RedWatch2022/src/main/java/frc/robot/commands/IntakeRun.java
@@ -22,13 +22,12 @@ public class IntakeRun extends CommandBase {
   @Override
   public void initialize() {
     m_intake.lowerIntake();
+    m_intake.intake();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_intake.intake();
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
