ddacb9f6b5ed01c18a36b60333307678bf25717c
JoshuaO22 <94143587+JoshuaO22@users.noreply.github.com>
2022-03-06 09:03:41 -0500

    Revert "Revert "Created 2 ball auto""
    This reverts commit c6f26fbe4b6e3d684f148e23e201143c4f13d728.

--- RedWatch2022/src/main/java/frc/robot/autoroutes/ThreeBallAuto.java
+++ RedWatch2022/src/main/java/frc/robot/autoroutes/ThreeBallAuto.java
@@ -5,39 +5,42 @@
 package frc.robot.autoroutes;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import static frc.robot.Constants.AutoRouteConstants.*;
-
 import frc.robot.commandgroups.ShootingRoutine;
 import frc.robot.commands.AutoForward;
-import frc.robot.commands.IntakeRun;
-import frc.robot.commands.LoadBallIntoMiddle;
 import frc.robot.commands.TurnAngle;
-import frc.robot.commands.VisionAlign;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Vision;
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoRoute2 extends SequentialCommandGroup {
-  /** Creates a new AutoRoute2. */
-  public AutoRoute2(Drivetrain drivetrain, Shooter shooter, Intake intake, Indexer indexer, Lights lights, Vision vision) {
-    super(
-      new AutoForward(40.695 + 5, drivetrain),
-      new VisionAlign(drivetrain, vision),
-      new ShootingRoutine(indexer, shooter, lights, vision.getRPM()),
-      new ShootingRoutine(indexer, shooter, lights, vision.getRPM()),
-
-      new TurnAngle(-drivetrain.getGyroAngle() + 46.12 - 10.792, drivetrain),
-      new AutoForward(159.865, drivetrain)
-      // intake
-    );
-
+public class ThreeBallAuto extends SequentialCommandGroup {
+  /** Creates a new ThreeBallAuto. */
+  public ThreeBallAuto(Drivetrain drivetrain, Shooter shooter, Intake intake, Indexer indexer, Lights lights) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      //shoot high from hub
+      new ShootingRoutine(indexer, shooter, lights, 2000),
+      
+      //ball 2
+      new TurnAngle(159, drivetrain),
+      new AutoForward(67.5, drivetrain),
+      new AutoForward(-15, drivetrain),
+
+      //ball 3
+      new TurnAngle(107, drivetrain),
+      new AutoForward(85, drivetrain),
+
+      //back to hub
+      new TurnAngle(146, drivetrain),
+      new AutoForward(86, drivetrain),
+      new TurnAngle(-50, drivetrain),
+      new ShootingRoutine(indexer, shooter, lights, 2000),
+      new ShootingRoutine(indexer, shooter, lights, 2000)
+    );
   }
 }
