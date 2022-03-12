3934b89df1a88d29aba60afadb49edf9077c56b3
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-03-11 19:50:37 -0500

    Update curvatureDrive.java

--- RedWatch2022/src/main/java/frc/robot/commands/curvatureDrive.java
+++ RedWatch2022/src/main/java/frc/robot/commands/curvatureDrive.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
@@ -41,7 +40,6 @@ public class curvatureDrive extends CommandBase {
     // drive with speeds of the parameter
     m_drivetrain.curvatureDrive(m_stickY.getAsDouble(), m_stickX.getAsDouble(), m_turnInPlace.getAsBoolean());
   }
-
 
   // Called once the command ends or is interrupted.
   @Override
