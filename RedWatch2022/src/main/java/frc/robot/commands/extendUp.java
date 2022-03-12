12c5c229541883134dc939a490efcb2ee6502201
MatthewEsaia <94143562+MatthewEsaia@users.noreply.github.com>
2022-03-11 19:51:16 -0500

    Update extendUp.java

--- RedWatch2022/src/main/java/frc/robot/commands/extendUp.java
+++ RedWatch2022/src/main/java/frc/robot/commands/extendUp.java
@@ -5,8 +5,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import static frc.robot.Constants.ClimberConstants.*;
 import frc.robot.subsystems.Climber;
+import static frc.robot.Constants.ClimberConstants.*;
 
 public class extendUp extends CommandBase {
 
