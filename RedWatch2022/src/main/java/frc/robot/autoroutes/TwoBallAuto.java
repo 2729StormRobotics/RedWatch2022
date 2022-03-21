5d08804be847fc6b121d5e73c7d7b79ef20e7da6
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2022-03-21 10:55:41 -0400

    updated two ball auto
    autonomy with two balls

--- RedWatch2022/src/main/java/frc/robot/autoroutes/TwoBallAuto.java
+++ RedWatch2022/src/main/java/frc/robot/autoroutes/TwoBallAuto.java
@@ -5,36 +5,47 @@
 package frc.robot.autoroutes;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commandgroups.IntakeIndex;
 import frc.robot.commandgroups.ShootingRoutine;
 import frc.robot.commands.AutoForward;
+import frc.robot.commands.IntakeLower;
+import frc.robot.commands.IntakeRaise;
 import frc.robot.commands.TurnAngle;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Vision;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class TwoBallAuto extends SequentialCommandGroup {
   /** Creates a new TwoBallAuto. */
-  public TwoBallAuto(Drivetrain drivetrain, Shooter shooter, Intake intake, Indexer indexer, Lights lights) {
+  public TwoBallAuto(Drivetrain drivetrain, Shooter shooter, Intake intake, Indexer indexer, Lights lights, Vision vision) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       //shoot ball 1
-      new ShootingRoutine(indexer, shooter, lights, 2000),
+      new IntakeLower(intake),
+      new WaitCommand(0.5),
+      new TurnAngle(-30, drivetrain),
+      new ShootingRoutine(indexer, shooter, lights, 2600),
+      new TurnAngle(210, drivetrain),
 
       //ball 2
-      new TurnAngle(159, drivetrain),
-      new AutoForward(67.5, drivetrain),
+      new IntakeIndex(drivetrain, intake, indexer, 55.0),
+      new IntakeRaise(intake),
+      new AutoForward(-55, drivetrain),
+      new TurnAngle(-210, drivetrain),
+      new ShootingRoutine(indexer, shooter, lights, 2600)
 
-      //hub
-      // new AutoForward(-67.5, drivetrain),
-      new TurnAngle(237, drivetrain),
-      new AutoForward(74, drivetrain),
-      new ShootingRoutine(indexer, shooter, lights, 2000)
+      // //hub
+      // // new AutoForward(-67.5, drivetrain),
+      // new TurnAngle(237, drivetrain),
+      // new AutoForward(74, drivetrain),
+      // new ShootingRoutine(indexer, shooter, lights, 2000)
     );
   }
 }
