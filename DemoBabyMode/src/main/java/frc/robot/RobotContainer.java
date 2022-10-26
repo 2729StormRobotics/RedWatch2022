fabc39891e04ad99148051739fa1ec9b6cbe2364
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2022-10-26 14:55:34 -0400

    added drivetrain
    drivetrain (drivetrain)

--- DemoBabyMode/src/main/java/frc/robot/RobotContainer.java
+++ DemoBabyMode/src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.net.PortForwarder;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.commands.curvatureDrive;
+import static frc.robot.Constants.IOPorts.*;
+import static frc.robot.Constants.LightConstants.*;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +30,44 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Intake m_intake;
+  private final Shooter m_shooter;
+  // private final Camera m_camera;
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Drivetrain m_drivetrain;
+
+  private final XboxController m_driver = new XboxController(kDriverController);
+  public final XboxController m_weapons = new XboxController(kWeaponsController);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // m_camera = new Camera();
+    m_intake = new Intake();
+    m_shooter = new Shooter();
+
+    m_drivetrain = new Drivetrain();
+
+
+    // Set up Control Panel
+
+    /**
+     * Default command for the climbers. 
+     * Left Joystick for telescoping arms, right for pivoting.
+     * Holding down the bumpers allows for individual control of the corresponding telescoping arm
+     */
+
+    /**
+     * Default command for the drivetrain
+     * Uses curvatureDrive method similar to arcade
+     * Left joystick for speed, right for turning
+     * Holding down left trigger allows turning in place
+     */ 
+    m_drivetrain.setDefaultCommand(
+      new curvatureDrive(() -> m_driver.getLeftY() / 1.25, () -> m_driver.getRightX() / 1.75, () -> Drivetrain.isTriggerPressed(m_driver.getLeftTriggerAxis()), m_drivetrain));
+
     // Configure the button bindings
-    configureButtonBindings();
+    configureButtonBindings();  
+
   }
 
   /**
@@ -34,7 +76,22 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    /**
+     * Button mappings for the weapons controller. Currently set to:
+     * A (when pressed) toggles the intake up/down
+     * Y (while held) aligns the robot to a vision target
+     * A (when pressed) drives the robot forward a set distance
+     */
+    // new JoystickButton(m_driver, Button.kA.value).whenPressed(new IntakeToggle(m_intake));
+    /**
+     * Button mappings for the weapons controller. Currently set to:
+     * X (while held) runs the intake motors
+     * B (when pressed) runs the index motor until the beambreak is broken
+     * Y (when pressed) runs the index then shoot command (index -> rev launcher based on distance -> shoot ball)
+     * A (while held) reverses the index motor to eject the ball
+     */
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -42,7 +99,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    // return new AutoRoute1(m_drivetrain, m_shooter, m_intake, m_indexer, m_lights, m_vision); // one ball auto off tarmac
+    return new curvatureDrive(() -> m_driver.getLeftY() / 1.25, () -> m_driver.getRightX() / 1.75, () -> Drivetrain.isTriggerPressed(m_driver.getLeftTriggerAxis()), m_drivetrain);
   }
 }
---
+++ DemoBabyMode/src/main/java/frc/robot/commands/curvatureDrive.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class curvatureDrive extends CommandBase {
+  
+  private final Drivetrain m_drivetrain;
+  private final DoubleSupplier m_stickY;
+  private final DoubleSupplier m_stickX;
+  private final BooleanSupplier m_turnInPlace;
+
+  /** Creates a new differentialDrive. */
+  public curvatureDrive(DoubleSupplier stickY, DoubleSupplier stickX, BooleanSupplier turnInPlace, Drivetrain subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = subsystem;
+    m_stickY = stickY;
+    m_stickX = stickX;
+    m_turnInPlace = turnInPlace;
+
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_drivetrain.stopDrive();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // drive with speeds of the parameter
+    m_drivetrain.curvatureDrive(Drivetrain.sqaureInput(m_stickY.getAsDouble()), Drivetrain.sqaureInput(m_stickX.getAsDouble()), m_turnInPlace.getAsBoolean());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    // stop drive
+    m_drivetrain.stopDrive();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // command only stops when another is called
+    return false;
+  }
+}
